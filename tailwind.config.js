b5be330a005519a79e61a55b09b3cb34a5dc9615
GutoAS <agostinhosoberano2@gmail.com>
2023-01-02 12:40:31 +0200

    Home section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        "3xl": "0px 4px 141px rgba(0, 0, 0, 0.25)",
+      },
+    },
   },
   plugins: [],
 };
